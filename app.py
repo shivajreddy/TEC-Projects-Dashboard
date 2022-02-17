468674e59017b98517076aa0e38aa8d8f7a41e53
Shiva Reddy <shivajayaramireddy@gmail.com>
2022-02-17 08:47:20 -0500

    added assets/images

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from distutils.command.config import config
 import pandas as pd
 import numpy as np
 import streamlit as st
@@ -5,7 +6,12 @@ import plotly.express as px
 from PIL import Image
 import datetime
 
-st.set_page_config(page_title='Survey Results')
+st.set_page_config(
+    page_title='Eagle Projects Dashboard',
+    layout="wide",
+    page_icon='assets/images/fav.png',
+)
+
 st.header('Eagle Projects Dashboard')
 st.subheader('Lot Specifics')
 
@@ -32,9 +38,9 @@ df = pd.read_excel(excel_file,
 excel_file = 'test2.xlsx'
 sheet_name = 'DATA'
 df_2 = pd.read_excel(excel_file,
-                                sheet_name=sheet_name,
-                               usecols='B:L',
-                               header=1)
+                    sheet_name=sheet_name,
+                    usecols='B:L',
+                    header=1)
 
 # Converting Date Time into Date
 df_2['Contract Date'] = pd.to_datetime(df_2['Contract Date']).dt.date
@@ -44,3 +50,8 @@ df_2['Actual'] = pd.to_datetime(df_2['Actual']).dt.date
 st.dataframe(df)
 st.subheader('TEST2.XLSX')
 st.dataframe(df_2)
+
+modeler = df_2['Assigned'].unique().tolist()
+times = df_2['Time'].tolist()
+
+# st.write(s)
